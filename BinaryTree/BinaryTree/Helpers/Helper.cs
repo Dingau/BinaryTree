02169e2815748632ed7b323e51d030a1e4910c42
Dingau <simas.lukosevicius@gmail.com>
2019-09-24 00:15:04 +0300

    Add files via upload

---
+++ BinaryTree/BinaryTree/Helpers/Helper.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BinaryTree.Helpers
+{
+    public class Helper
+    {
+        internal static string[] MakeArrayOfRows(string input)
+        {
+            return input.Split(Environment.NewLine);
+        }
+
+        internal static int[,] MakeTableFromRows(string[] arrOfRows)
+        {
+            int[,] tableToReturn = new int[arrOfRows.Length, arrOfRows.Length + 1];
+
+            //rows
+            for (int i = 0; i < arrOfRows.Length; i++)
+            {
+                string[] rowChars = arrOfRows[i].Trim().Split(' ');
+
+                //collumns
+                for (int j = 0; j < rowChars.Length; j++)
+                {
+                    int number;
+                    int.TryParse(rowChars[j], out number);
+                    tableToReturn[i, j] = number;
+                }
+            }
+
+            return tableToReturn;
+        }
+
+        internal static List<int> FindMaximumPath(int[,] table)
+        {
+            int tableLength = table.GetLength(0);
+            int attempts = (int)Math.Pow(2, tableLength - 1);
+            List<int> maxSumPath = new List<int> ();
+            int index;
+
+            for (int i = 0; i <= attempts; i++)
+            {
+                List<int> tempSumPath = new List<int> { table[0, 0] };
+                index = 0;
+                
+                for (int j = 0; j < tableLength - 1; j++)
+                {
+                    index = index + (i >> j & 1);
+                    int tableVal = table[j + 1, index];
+                    if (CheckWithParentEvenOrOdd(tempSumPath[tempSumPath.Count - 1], tableVal))
+                    {
+                        tempSumPath.Add(tableVal);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                if (tempSumPath.Count == tableLength && tempSumPath.Sum() > maxSumPath.Sum())
+                {
+                    maxSumPath = tempSumPath;
+                }
+            }
+            return maxSumPath;
+        }
+
+        private static bool CheckWithParentEvenOrOdd(int parent, int tableVal)
+        {
+            bool toReturn = false;
+            if (parent%2 != tableVal%2)
+            {
+                toReturn = true;
+            }
+            return toReturn;
+        }
+
+      
+    }
+}
---
+++ BinaryTree/BinaryTree/Program.cs
@@ -0,0 +1,44 @@
+﻿using BinaryTree.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrOfRows = Helper.MakeArrayOfRows(Constants.SampleImput);
+            int[,] table = Helper.MakeTableFromRows(arrOfRows);
+            List<int> maxSumPath = Helper.FindMaximumPath(table);
+
+            PrintResult(maxSumPath);
+            Console.ReadLine();
+        }
+
+        private static void PrintResult(List<int> maxSumPath)
+        {
+            if (maxSumPath.Count != 0)
+            {
+                Console.WriteLine($"Maximum sum: {maxSumPath.Sum()}");
+                Console.WriteLine($"Path is: {GetPath(maxSumPath)}");
+            }
+            else
+            {
+                Console.WriteLine("There is no valid path from top to bottom of pyramid!");
+            }
+            
+        }
+
+        private static string GetPath(List<int> maxSumPath)
+        {
+            string path = string.Empty;
+            foreach (var spot in maxSumPath)
+            {
+                path = string.Format($"{path}{spot}, ");
+            }
+            return path.Remove(path.Trim().Length - 1);
+        }
+    }
+}
